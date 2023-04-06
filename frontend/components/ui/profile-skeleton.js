c2a04ba5679b6b9c25333b63c5631ca75cf37b74
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-06 19:17:40 -0400

    ADDED: some skeleton changes to profile page

--- frontend/components/ui/profile-skeleton.js
+++ frontend/components/ui/profile-skeleton.js
@@ -9,7 +9,7 @@ export default function ProfileSkeleton() {
             <div className="flex items-center space-x-3 lg:pl-2">
               <div className="bg-gray-200 dark:bg-[#333] w-8 h-8 animate-pulse rounded-md" />
 
-              <div className="bg-gray-200 dark:bg-[#333] w-96 h-8 animate-pulse rounded-md" />
+              <div className="bg-gray-200 dark:bg-[#333] w-28 h-4 animate-pulse rounded-md" />
             </div>
           </td>
           <td className="px-6 py-3 text-sm font-medium text-gray-500">
@@ -17,15 +17,15 @@ export default function ProfileSkeleton() {
               <div className="flex flex-shrink-0 -space-x-1" />
 
               <span className="flex-shrink-0 text-xs font-medium leading-5">
-                <div className="bg-gray-200 dark:bg-[#333] w-8 h-8 animate-pulse rounded-md" />
+                <div className="bg-gray-200 dark:bg-[#333] w-4 h-4 animate-pulse rounded-md" />
               </span>
             </div>
           </td>
           <td className="hidden whitespace-nowrap px-6 py-3 text-right text-sm text-gray-500 md:table-cell">
-            <div className="bg-gray-200 dark:bg-[#333] w-24 h-8 animate-pulse rounded-md" />
+            <div className="bg-gray-200 dark:bg-[#333] w-20 h-4 animate-pulse rounded-md" />
           </td>
           <td className="whitespace-nowrap px-6 py-3 text-right text-sm font-medium">
-            <div className="bg-gray-200 dark:bg-[#333] w-36 h-8 animate-pulse rounded-md" />
+            <div className="bg-gray-200 dark:bg-[#333] w-36 h-9 animate-pulse rounded-md" />
           </td>
         </tr>
       ))}
--- frontend/components/ui/profile-top-skeleton.js
+++ frontend/components/ui/profile-top-skeleton.js
@@ -19,12 +19,12 @@ export default function ProfileTopSkeleton() {
           <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 dark:border-[#333] bg-white dark:bg-[#111]">
             <div className="flex-1 truncate px-4 py-2 text-sm">
               <div className="space-y-1">
-                <div className="bg-gray-200 dark:bg-[#333] w-16 h-4 animate-pulse rounded-md" />
-                <div className="bg-gray-200 dark:bg-[#333] w-16 h-4 animate-pulse rounded-md" />
+                <div className="bg-gray-200 dark:bg-[#333] w-full h-4 animate-pulse rounded-md" />
+                <div className="bg-gray-200 dark:bg-[#333] w-10 h-4 animate-pulse rounded-md" />
               </div>
             </div>
 
-            <div className="mr-4 bg-gray-200 dark:bg-[#333] w-8 h-8 animate-pulse rounded-md" />
+            <div className="mr-4 bg-gray-200 dark:bg-[#333] w-2 h-6 animate-pulse rounded-md" />
           </div>
         </li>
       ))}
