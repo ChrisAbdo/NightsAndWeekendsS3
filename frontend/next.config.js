cb499cc64fdebd4f6d15eebb06c08b056a1e8bf8
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-06 18:49:15 -0400

    ADDED: Fixed env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.NEXT_PUBLIC_ANALYZE === 'true',
 })
 
 /** @type {import('next').NextConfig} */
