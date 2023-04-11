2aaf114ef36a68f86d204223470c700d3c3a89fd
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-10 23:32:05 -0400

    ADDED: next head tag to app layout

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/src/styles/globals.css";
 
+import Head from "next/head";
+
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 
@@ -8,7 +10,7 @@ import { Source_Code_Pro } from "next/font/google";
 import dynamic from "next/dynamic";
 const Navbar = dynamic(() => import("@/components/shared-layout/navbar"), {
   loading: () => <p>Loading...</p>,
-})
+});
 
 const sourceCodePro = Source_Code_Pro({
   weight: "400",
@@ -18,6 +20,11 @@ const sourceCodePro = Source_Code_Pro({
 export default function App({ Component, pageProps }) {
   return (
     <main className={sourceCodePro.className}>
+      <Head>
+        <title>Etherwav</title>
+        <meta name="description" content="Etherwav" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <ThemeProvider defaultTheme="light">
         <div className="bg-white dark:bg-black">
           <Navbar />
