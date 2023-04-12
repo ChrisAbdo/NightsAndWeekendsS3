d07abfbf6deb6c430fa3e3b26a7d93f95d5b55ad
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-12 17:58:48 -0400

    does it show up now

--- frontend/components/index-ui/ascii-fire.js
+++ frontend/components/index-ui/ascii-fire.js
@@ -1,69 +1,59 @@
-import React, { useLayoutEffect, useRef, useState } from "react";
-import { motion, useAnimation } from "framer-motion";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 const AsciiFireAnimation = () => {
   const preRef = useRef(null);
   const controls = useAnimation();
-  const [width, setWidth] = useState(() => {
-    if (typeof window !== "undefined") {
-      return Math.floor(window.innerWidth / 5);
-    }
-    return 80;
-  });
+  const [width, setWidth] = useState(() =>
+    typeof window !== 'undefined' ? Math.floor(window.innerWidth / 5) : 80
+  );
 
-  useLayoutEffect(() => {
-    if (typeof window === "undefined") return;
-
-    const updateWidth = () => {
-      setWidth(Math.floor(window.innerWidth / 5));
-    };
-
-    const debounce = (func, wait) => {
-      let timeout;
-      return (...args) => {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-          func.apply(this, args);
-        }, wait);
-      };
-    };
-
-    const debouncedUpdateWidth = debounce(updateWidth, 250);
-
-    window.addEventListener("resize", debouncedUpdateWidth);
-
-    return () => {
-      window.removeEventListener("resize", debouncedUpdateWidth);
-    };
+  const updateWidth = useCallback(() => {
+    setWidth(Math.floor(window.innerWidth / 5));
   }, []);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    window.addEventListener('resize', updateWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateWidth);
+    };
+  }, [updateWidth]);
+
+  const flameAnimation = useCallback(() => {
     const height = 25;
     const size = width * height;
-    const b = [];
-    for (let i = 0; i < size + width + 1; i++) b[i] = 0;
-    const char = " ethrwav".split("");
+    const buffer = Array(size + width + 1).fill(0);
+    const char = ' ethrwav'.split('');
 
-    function f() {
+    function flameIteration() {
       for (let i = 0; i < 10; i++)
-        b[Math.floor(Math.random() * width) + width * (height - 1)] = 70;
-      let a = "";
+        buffer[Math.floor(Math.random() * width) + width * (height - 1)] = 70;
+
+      let animationFrame = '';
       for (let i = 0; i < size; i++) {
-        b[i] = Math.floor(
-          (b[i] + b[i + 1] + b[i + width] + b[i + width + 1]) / 4
+        buffer[i] = Math.floor(
+          (buffer[i] + buffer[i + 1] + buffer[i + width] + buffer[i + width + 1]) / 4
         );
-        a += char[b[i] > 7 ? 7 : b[i]];
-        if (i % width > width - 2) a += "\r\n";
+        animationFrame += char[buffer[i] > 7 ? 7 : buffer[i]];
+        if (i % width > width - 2) animationFrame += '\r\n';
       }
 
       if (preRef.current) {
-        preRef.current.firstChild.data = a;
+        preRef.current.firstChild.data = animationFrame;
       }
-      requestAnimationFrame(f);
+      requestAnimationFrame(flameIteration);
     }
-    f();
-    controls.start({ opacity: 1, transition: { duration: 10 } });
+
+    flameIteration();
+    controls.start({ opacity: 1, transition: { duration: 4 } });
   }, [width]);
+
+  useEffect(() => {
+    flameAnimation();
+  }, [flameAnimation]);
 
   return (
     <div className="overflow-hidden">
@@ -73,7 +63,7 @@ const AsciiFireAnimation = () => {
         initial={{ opacity: 0 }}
         animate={controls}
       >
-        Javascript is required to view this dope animation.
+        JavaScript is required to view this dope animation.
       </motion.pre>
     </div>
   );
