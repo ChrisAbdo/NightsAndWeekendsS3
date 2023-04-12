011bd54c93dc6843df371581210e7906e385ce5f
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-12 15:54:54 -0400

    ADDED: Fixed buttons for layout shift in navbar

--- frontend/components/shared-layout/navbar.js
+++ frontend/components/shared-layout/navbar.js
@@ -46,31 +46,11 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const web3 = new Web3(Web3.givenProvider);
 
   const [connectedAccount, setConnectedAccount] = useState("");
-
-  // useEffect(() => {
-  //   const loadConnectedAccount = async () => {
-  //     const accounts = await web3.eth.getAccounts();
-  //     if (accounts.length > 0) {
-  //       setConnectedAccount(accounts[0]);
-  //     }
-  //   };
-
-  //   // event listener for MetaMask account change
-  //   window.ethereum.on("accountsChanged", function (accounts) {
-  //     setConnectedAccount(accounts[0]);
-  //   });
-
-  //   // event listener for MetaMask disconnect
-  //   window.ethereum.on("disconnect", function (error) {
-  //     setConnectedAccount(null);
-  //   });
-
-  //   loadConnectedAccount();
-  // }, []);
 
   useEffect(() => {
     if (typeof window === "undefined") return; // This checks if the window object is available
@@ -80,6 +60,7 @@ export default function Navbar() {
       if (accounts.length > 0) {
         setConnectedAccount(accounts[0]);
       }
+      setLoading(false); // Set loading to false after the account is loaded or no account is found
     };
 
     // event listener for MetaMask account change
@@ -240,7 +221,7 @@ export default function Navbar() {
           </Link>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          {connectedAccount && (
+          {/* {connectedAccount && (
             <button
               type="button"
               onClick={() => setOpen(true)}
@@ -258,7 +239,47 @@ export default function Navbar() {
             >
               <span>Connect Wallet</span>
             </button>
-          )}
+          )} */}
+          <div className="flex items-center justify-center w-40">
+            {loading ? (
+              <button
+                type="button"
+                onClick={() => setOpen(true)}
+                className="w-full rounded-md bg-transparent px-5 py-2 text-sm font-semibold text-black dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-[#333] hover:bg-gray-200 dark:hover:bg-[#111] transition duration-200 flex items-center justify-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-loader-2 animate-spin"
+                >
+                  <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
+                </svg>
+              </button>
+            ) : connectedAccount ? (
+              <button
+                type="button"
+                onClick={() => setOpen(true)}
+                className="w-full rounded-md bg-transparent px-5 py-2 text-sm font-semibold text-black dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-[#333] hover:bg-gray-200 dark:hover:bg-[#111] transition duration-200 flex items-center justify-center"
+              >
+                {connectedAccount.slice(0, 5)}...{connectedAccount.slice(-4)}
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => setOpen(true)}
+                className="w-full rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-[#333] hover:bg-gray-200 dark:hover:bg-[#111] transition duration-200 flex items-center justify-center"
+              >
+                <span>Connect Wallet</span>
+              </button>
+            )}
+          </div>
 
           <div className="flex items-center ml-4">
             <DarkModeToggle />
